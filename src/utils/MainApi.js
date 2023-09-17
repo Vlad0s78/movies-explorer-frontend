7b2351962de563dcb10c7a6c07c77701e7855393
Vlad Cheshev <vlad.toloka37@yandex.ru>
2023-09-17 18:38:26 +0300

    hotfix: base_url

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://api.filmhub.nomoreparties.co";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://api.filmhub.nomoreparties.co";
+// const BASE_URL = "http://localhost:3000";
 
 function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`ОшибОЧКА: ${res.status}`);
